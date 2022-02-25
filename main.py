6aeb2d1619e7f41070c1b46218f47be2de12ca7d
Krzysztof Baran <krysbaran@gmail.com>
2022-02-25 10:43:15 +0100

    Add number paprameter

--- main.py
+++ main.py
@@ -18,17 +18,29 @@ parser.add_argument('--p', '-path', type=str,
                     help=f"Path to the Power Log executable (default={DEFAULT_POWERLOG})",
                     default=DEFAULT_POWERLOG)
 parser.add_argument('--f', '-file', type=str, help='File path for saving the results to a csv file (default=PWR.csv)',
-                    default='PWR.csv')
+                    default='PWR')
+parser.add_argument('--n', type=int, help='Number of times to run the power gadget tool', default=1)
 parser.add_argument('--s', '-seconds', type=int, help='Number of seconds to run power gadget tool')
 parser.add_argument('--cmd', '-command', type=str, help='Command to run the tests on')
 
+
 args = vars(parser.parse_args())
+
+times_to_run = args.get('n')
+
+if times_to_run < 1 :
+    raise Exception('n must be at least 1')
 
 duration_str = f"-duration {args.get('s')}" if args.get("s") is not None else ""
 command_str = f"-cmd {args.get('cmd')}" if args.get("cmd") is not None else ""
 
-cmd = f'"{args.get("p")}\\PowerLog3.0.exe" {duration_str} -file {args.get("f")} {command_str}'
-print("Running:", cmd, "...")
+print(f"RUN PowerLog3.0 {times_to_run} times...\n")
 
-os.system(cmd)
-print("DONE")
+ZFILL_NUM = len(str(times_to_run))
+
+for i in range(times_to_run):
+    cmd = f'"{args.get("p")}\\PowerLog3.0.exe" {duration_str} -file {args.get("f")}_{str(i+1).zfill(ZFILL_NUM)}.csv {command_str}'
+    print("Running:", cmd, "\n")
+
+    os.system(cmd)
+    print("DONE\n")
